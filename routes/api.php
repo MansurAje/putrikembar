2ac14c53edf02eb54f0a44e4dc99e039d7ff7e50
MansurAje <m_aje@yahoo.co.id>
2021-06-28 17:37:41 +0700

    routes api

--- routes/api.php
+++ routes/api.php
@@ -25,3 +25,7 @@ Route::group(['middleware' => 'auth:api'], function(){
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/t', function () {
+    return 'halaman test';
+});
