20228b03b105c3cce9b149dd5e6c2b01d3060be5
MansurAje <m_aje@yahoo.co.id>
2021-06-28 15:15:00 +0700

    platform check

--- vendor/composer/platform_check.php
+++ vendor/composer/platform_check.php
@@ -4,13 +4,8 @@
 
 $issues = array();
 
-<<<<<<< HEAD
-if (!(PHP_VERSION_ID >= 70205)) {
-    $issues[] = 'Your Composer dependencies require a PHP version ">= 7.2.5". You are running ' . PHP_VERSION . '.';
-=======
 if (!(PHP_VERSION_ID >= 70100)) {
     $issues[] = 'Your Composer dependencies require a PHP version ">= 7.1.0". You are running ' . PHP_VERSION . '.';
->>>>>>> 802a63f33bede87611c0aadc9f2873dbf6dd1165
 }
 
 if ($issues) {
