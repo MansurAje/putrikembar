b99080418f8c5425a37df40410e0899048070eee
MansurAje <m_aje@yahoo.co.id>
2021-06-29 11:08:06 +0700

    routes

--- routes/api.php
+++ routes/api.php
@@ -15,11 +15,19 @@ use Illuminate\Support\Facades\Route;
 */
 
 
-Route::group(['prefix' => 'v1'], function () {
+/* Route::group(['prefix' => 'v1'], function () {
     Route::post('login', 'API\UserController@login');
     Route::post('register', 'API\UserController@register');
     Route::get('logout', 'API\UserController@logout')->middleware('auth:api');
     Route::get('details', 'API\UserController@details')->middleware('auth:api');
+}); */
+
+Route::post('login', 'API\UserController@login');
+    Route::post('register', 'API\UserController@register');
+
+Route::group(['middleware' => 'auth:api'], function(){
+	Route::post('details', 'API\UserController@details');
+    Route::get('logout', 'API\UserController@logout');
 });
 
 
