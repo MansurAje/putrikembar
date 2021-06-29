c6ac973197b307d34e4bb03a41a8723c0a1e17e6
MansurAje <m_aje@yahoo.co.id>
2021-06-29 10:36:30 +0700

    follow instalation fluter

--- config/auth.php
+++ config/auth.php
@@ -43,7 +43,7 @@ return [
 
         'api' => [
             //'driver' => 'token',
-            'driver' => 'password',
+            'driver' => 'passport',
             'provider' => 'users',
             'hash' => false,
         ],
